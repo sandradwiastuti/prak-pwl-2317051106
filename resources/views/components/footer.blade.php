ff59254cbca89dc26f960e258a3fc25be4f8167d
SANDRA DWI ASTUTI <2317051106@students.unila.ac.id>
2025-10-07 11:41:55 +0700

    Selesai Langkah Praktikum Modul 5

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-white text-center py-3 mt-auto" style="background: linear-gradient(90deg, #db7bc3ff, #062452ff);">
-  <p class="mb-0">© 2025 Tugas Web Lanjut - Universitas Lampung</p> 
+  <p class="mb-0">© 2025 Web Lanjut - Universitas Lampung</p> 
 </footer>
